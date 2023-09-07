df98556d5e37d524991940c88877e77fdd9cb38e
arppy <berta@inf.u-szeged.hu>
2023-09-07 10:27:12 +0200

    optim prior after feature

--- create_input_by_optim_feature_and_prior.py
+++ create_input_by_optim_feature_and_prior.py
@@ -204,4 +204,66 @@ for idx, batch in enumerate(reference_images) :
 				(alpha * opt + beta * opt2).backward()
 				optimizer.step()
 			if options.verbose :
-				print(target_label,i,opt.item(),opt2.item())
+				print(target_label,"0",i,opt.item(),opt2.item())
+		activation_to_optimize.requires_grad = False
+		net_input = get_noise(input_depth, 'noise', imsize_net).type(dtype).detach()
+		net_input = net_input.to(DEVICE)
+		net_input_saved = net_input.detach().clone()
+		net_input_saved = net_input_saved.to(DEVICE)
+		noise = net_input.detach().clone()
+		noise = noise.to(DEVICE)
+		net = skip(input_depth, 3, num_channels_down = [16, 32, 64, 128, 128, 128],
+								   num_channels_up =   [16, 32, 64, 128, 128, 128],
+								   num_channels_skip = [0, 4, 4, 4, 4, 4],
+								   filter_size_down = [5, 3, 5, 5, 3, 5], filter_size_up = [5, 3, 5, 3, 5, 3],
+								   upsample_mode='bilinear', downsample_mode='avg',
+								   need_sigmoid=True, pad=pad, act_fun='LeakyReLU').type(dtype)
+		net = net.to(DEVICE)
+		#s  = sum(np.prod(list(pp.size())) for pp in net.parameters())
+		#print ('Number of params: %d' % s)
+		#print("shape",net(net_input).shape) #torch.Size([1, 3, 256, 256])
+		pp = get_params(OPT_OVER, net, net_input)
+		if options.cosine_learning :
+			optimizer = torch.optim.AdamW(pp, lr=options.learning_rate, weight_decay=1e-4)
+			scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=options.learning_rate, total_steps=None,
+														epochs=options.num_iters, steps_per_epoch=1, pct_start=options.pct_start,
+														anneal_strategy='cos', cycle_momentum=False, div_factor=1.0,
+														final_div_factor=1000000000.0, three_phase=False, last_epoch=-1, verbose=False)
+		else :
+			optimizer = torch.optim.Adam([{'params': pp, 'lr': options.learning_rate}])
+		for i in range(iternum+1):
+			optimizer.zero_grad()
+			if param_noise:
+				for n in [x for x in net.parameters() if len(x.size()) == 4]:
+					n = n + n.detach().clone().normal_() * n.std()/50
+			net_input = net_input_saved
+			if reg_noise_std > 0:
+				net_input = net_input_saved + (noise.normal_() * reg_noise_std)
+			X = net(net_input)[:, :, :imsize, :imsize]
+			logits = model_poisoned(transformNorm(X))
+			activations_image_optimized = torch.flatten(activation_extractor.pre_activations[options.layer_name], start_dim=1, end_dim=-1)
+			pred = torch.nn.functional.softmax(logits, dim=1)
+			pred_by_target = pred[range(pred.shape[0]), target_label]
+			#opt = torch.sum(pred_by_target)
+			opt = rem(logits,target_label).logsumexp(1)-logits[:,target_label]
+			cossim = cos_sim(activations_image_optimized, activation_to_optimize)
+			cossim2 = cos_sim(activations_image_optimized, activations_reference_images)
+			opt2 = torch.sum(cossim)
+			opt3 = torch.sum(cossim2)
+			if i<iternum:
+				opt2.backward()
+				optimizer.step()
+				if options.cosine_learning:
+					scheduler.step()
+			if options.verbose :
+				print(target_label,"1",i,opt.item(),opt2.item(),opt3.item())
+
+		model_based_dir_name = options.model.rsplit('/', 1)[1]
+		try:
+			os.makedirs(os.path.join(options.out_dir_name, model_based_dir_name))
+		except FileExistsError:
+			pass
+		for i in range(len(X)):
+			if pred[i,target_label] > 0.8:
+				filename = str(target_label) + "_" + str(pred[i,target_label].item())[0:6] + "_" + str(cossim2[i].item())[0:6] + "_" + str(random.randint(1000000, 9999999)) + ".png"
+				save_image(X[i].clamp(0,1), os.path.join(options.out_dir_name, model_based_dir_name, filename))
