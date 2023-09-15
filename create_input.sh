b3488de9ceb7fbc696cd41fbc5ddb9904d8ba529
arppy <berta@inf.u-szeged.hu>
2023-09-15 13:51:52 +0200

    different_reference_images_per_class in create_input

--- create_input.sh
+++ create_input.sh
@@ -14,12 +14,12 @@ num_iters=$6 #100
 num_images_per_class=$7 #10
 alpha=$8 #1.0
 beta=$9 #0.001
-early_stopping=${10} #false
+a_bool=${10} #false for early_stopping or different_reference_images_per_class
 gpu=${11}
 
 for model_name in $(ls $model_dir*.pth) ; do
   if [ "$method" == "optim_prior" ]; then
-    if [ "$early_stopping" = true ]; then
+    if [ "$a_bool" = true ]; then
       python create_input_by_optim_prior.py --model $model_name --verbose --learning_rate $learning_rate --pct_start $pct_start --out_dir_name $out_dir --num_iters $num_iters --num_images_per_class $num_images_per_class --gpu $gpu --early_stopping
     else
       python create_input_by_optim_prior.py --model $model_name --verbose --learning_rate $learning_rate --pct_start $pct_start --out_dir_name $out_dir --num_iters $num_iters --num_images_per_class $num_images_per_class --gpu $gpu
@@ -27,6 +27,10 @@ for model_name in $(ls $model_dir*.pth) ; do
   elif [ "$method" == "optim_prior_moving_away_from_reference_image" ]; then
     python create_input_by_optim_prior_and_moving_away.py --layer_name "linear" --model $model_name --alpha $alpha --beta $beta --verbose --learning_rate $learning_rate --pct_start $pct_start --out_dir_name $out_dir --num_iters $num_iters --num_images_per_class $num_images_per_class --gpu $gpu
   elif [ "$method" == "optim_feature_and_prior" ]; then
-    python create_input_by_optim_feature_and_prior.py --layer_name "linear" --model $model_name --alpha $alpha --beta $beta --verbose --learning_rate $learning_rate --pct_start $pct_start --out_dir_name $out_dir --num_iters $num_iters --num_images_per_class $num_images_per_class --gpu $gpu
+    if [ "a_bool" = true ]; then
+      python create_input_by_optim_feature_and_prior.py --layer_name "linear" --model $model_name --alpha $alpha --beta $beta --verbose --learning_rate $learning_rate --pct_start $pct_start --out_dir_name $out_dir --num_iters $num_iters --num_images_per_class $num_images_per_class --gpu $gpu --different_reference_images_per_class
+    else
+      python create_input_by_optim_feature_and_prior.py --layer_name "linear" --model $model_name --alpha $alpha --beta $beta --verbose --learning_rate $learning_rate --pct_start $pct_start --out_dir_name $out_dir --num_iters $num_iters --num_images_per_class $num_images_per_class --gpu $gpu
+    fi;
   fi;
 done
