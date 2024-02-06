9b910e9960e95429dfe764f8fd0298f19dd0f202
arppy <berta@inf.u-szeged.hu>
2024-02-06 14:58:50 +0100

    remove negatives

--- create_input_by_first_optim_feature_and_next_image_to_feature.py
+++ create_input_by_first_optim_feature_and_next_image_to_feature.py
@@ -416,12 +416,12 @@ for target_label in dict_training_features:
 				scheduler.step()
 
 			activation_to_optimize.requires_grad = False
-			#torch.fmax(activation_to_optimize,torch.ones(1).to(DEVICE),out=activation_to_optimize)
+			torch.fmax(activation_to_optimize,torch.ones(1).to(DEVICE),out=activation_to_optimize)
 			torch.nn.functional.normalize(activation_to_optimize[0], p=2.0, dim=0, eps=1e-12, out=activation_to_optimize[0])
 			activation_to_optimize[0] *= torch.linalg.vector_norm(distant_images_activations[0], ord=2, dim=0, keepdim=True)
 
 			if options.verbose:
-				print(target_label, "0", i, pred_by_target.item(), opt.item(), opt2.item(), activation_to_optimize[0].shape, distant_images_activations[0].shape, end=' ')
+				print(target_label, "0", i, pred_by_target.item(), opt.item(), opt2.item(), end=' ')
 				if options.cosine_learning:
 					print("lr:", scheduler.get_last_lr()[0])
 				else:
